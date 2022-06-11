ccdc7f84c029636ebc7237a3a815f0439a4fabae
rachtibat <r.achtibat@outlook.de>
2022-06-11 02:25:38 +0200

    provides ImageNet download functionality

--- tutorials/VGG16_ImageNet/download_imagenet.py
+++ tutorials/VGG16_ImageNet/download_imagenet.py
@@ -1,5 +1,7 @@
 import urllib.request
 from tqdm import tqdm
+from pathlib import Path
+from typing import Union
 
 IMAGES_URL = "https://datacloud.hhi.fraunhofer.de/s/RjnK4badZgG7gMq/download"
 KIT_URL = "https://datacloud.hhi.fraunhofer.de/s/iz2MYBiGFCpxfg5/download"
@@ -12,17 +14,29 @@ class DownloadProgressBar(tqdm):
         self.update(b * bsize - self.n)
 
 
-def download_imagnet(url, output_path):
+def download(output_path: Union[str, Path]):
     """
-    Functionality not tested!
+    Downloads the ImageNet validation dataset from Fraunhofer HHI server. 
+    We provide the dataset only for testing purposes for the tutorial! By downloading you aggree to the terms 
+    of access listed on https://image-net.org/download.php.
+
+    Paramters:
+    ---------
+        output_path: str, pathlib.Path
+            parent folder of ImageNet val dataset and development kit.
     """
+
+    if type(output_path) == str:
+        output_path = Path(output_path)
+        output_path.mkdir(parents=True, exist_ok=True)
+
     print("By downloading you aggree to the terms of access listed on https://image-net.org/download.php")
     print("Start downloading ImageNet development kit")
-    with DownloadProgressBar(unit='B', unit_scale=True, miniters=1, desc=url.split('/')[-1]) as t:
+    with DownloadProgressBar(unit='B', unit_scale=True, miniters=1, desc=KIT_URL.split('/')[-1]) as t:
         urllib.request.urlretrieve(
-            KIT_URL, filename=output_path, reporthook=t.update_to)
+            KIT_URL, filename=output_path / Path("ILSVRC2012_devkit_t12.tar.gz"), reporthook=t.update_to)
 
     print("Start downloading ImageNet validation dataset")
-    with DownloadProgressBar(unit='B', unit_scale=True, miniters=1, desc=url.split('/')[-1]) as t:
+    with DownloadProgressBar(unit='B', unit_scale=True, miniters=1, desc=IMAGES_URL.split('/')[-1]) as t:
         urllib.request.urlretrieve(
-            IMAGES_URL, filename=output_path, reporthook=t.update_to)
+            IMAGES_URL, filename=output_path / Path("ILSVRC2012_img_val.tar"), reporthook=t.update_to)
