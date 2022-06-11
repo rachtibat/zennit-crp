61db8ac621dffeab7fcbc0fdc52455a9413fe45d
rachtibat <r.achtibat@outlook.de>
2022-06-11 02:24:25 +0200

    Clean up the main interface of 'click'. 'click' functionality might be provided in a future release.

--- crp/__main__.py
+++ crp/__main__.py
@@ -1,7 +1,8 @@
 import click
 
+# TODO: interface for distributed computation
 
-@click.group()
+
 def cli():
     print(r"""
    ___ ___ ___ 
@@ -11,28 +12,6 @@ def cli():
                
 """)
 
-
-@click.command()
-@click.argument('path')  # help='path to filename that contains the CRP inferface')
-def analyze(path):
-    """
-    Run Feature Visualization.
-
-    PATH points to filename that contains the CRP interface
-    """
-
-    print("FV running")
-
-
-@click.command()
-@click.argument('path')  # help='path to textfile that contains the paths to checkpoints')
-def collect(path):
-
-    print("collcet running")
-
-
-cli.add_command(analyze)
-cli.add_command(collect)
 
 if __name__ == '__main__':
     cli()
