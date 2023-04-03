489f7b194aae46e1a424c8c8bb643eb3cec7b1f6
rachtibat <r.achtibat@outlook.de>
2023-04-03 16:36:22 +0200

    update version

--- setup.py
+++ setup.py
@@ -40,7 +40,7 @@ def get_long_description(project_path):
 
 setup(
     name='zennit-crp',
-    version='0.5.3',
+    version='0.6.0',
     description='Concept Relevance Propagation and Relevance Maximization',
     author='Reduan Achtibat',
     license='BSD 3-Clause Clear License',
